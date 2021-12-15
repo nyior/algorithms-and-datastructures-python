d3fbba57e542f0c780919dd64460c3681a2e880a
Nyior <nyior.clement@aun.edu.ng>
2021-12-15 17:35:24 +0100

    Sorting: implement solution to insertion sort exercise

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -36,6 +36,49 @@ class InsertionSort():
             while j>0 and self.nums[j-1] > self.nums[j]:
                 self._swap(j-1, j)
                 j -= 1
-                
+
     def _swap(self, i, j) -> None:
         self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+
+
+
+""""
+    We have seen how to implement insertion sort with integers. 
+    Let's use insertion sort with custom objects. Create a Person class with 2 instance 
+    variables: name and age. Let's sort a list of Person objects in ascending order based on their ages.
+"""
+
+class Person:
+ 
+    # we have 2 instance variables - name and age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+ 
+    # we have to define the 'less than' function to let Python know how to interpret the < and > operators
+    # when we use < then Python calls this function to decide what object is smaller and what is greater
+    def __lt__(self, other):
+        return self.age < other.age
+ 
+    # string representation of the object (when we use the print() function then this function is called)
+    def __repr__(self):
+        return str(self.name)
+ 
+ 
+# we can use the exact same insertion sort algorithm we have implemented
+def insertion_sort(people):
+ 
+    for i in range(len(people)):
+ 
+        j = i
+ 
+        while j > 0 and people[j - 1] > people[j]:
+            people[j], people[j-1] = people[j-1], people[j]
+            j = j - 1
+ 
+ 
+if __name__ == '__main__':
+ 
+    n = [Person('Adam', 23), Person('Ana', 17), Person('Kevin', 32), Person('Daniel', 37)]
+    insertion_sort(n)
+    print(n)
