ba1aa818f6025fe720456033534e2800567d27ba
Nyior <nyior.clement@aun.edu.ng>
2022-02-24 20:29:01 +0100

    add reverse string recursive solution

--- algorithms/recursion/reverse_string.py
+++ algorithms/recursion/reverse_string.py
@@ -1,4 +1,16 @@
 """
-write a reverse function that reverses
-a string. So, if the function accepts the argument "abcde", it’ll return "edcba".
+    write a reverse function that reverses
+    a string. So, if the function accepts the argument "abcde", it’ll return "edcba".
+
+    - what is the sub-problem: arr[-1] + reverse(arr[0, -1])
 """
+
+def reverse_string(string):
+    if len(string) == 1:
+        return string[-1]
+    else:
+        return string[-1] + reverse_string(string[0:-1])
+
+
+if __name__ == '__main__':
+    print(reverse_string("clement"))
